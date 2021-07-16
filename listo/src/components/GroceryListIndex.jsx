2d1bc1bb8c3826dec6a50327f8fd37523893de89
Ryan Modrall <ryanemccall20@gmail.com>
2021-07-15 20:45:35 -0400

    Updating the GroceryListIndex so we can see things

--- listo/src/components/GroceryListIndex.jsx
+++ listo/src/components/GroceryListIndex.jsx
@@ -1,10 +1,31 @@
-const GroceryListIndex = () => {
+import { useState } from "react";
+import { Container, Row, Col } from "reactstrap";
+import GroceryListUpdate from "./GroceryListUpdate";
+import GroceryListDelete from "./GroceryListDelete";
+import GroceryListCreate from "./GroceryListCreate";
+import GroceryListGet from "./GroceryListGet";
 
+const GroceryListIndex = () => {
+    const [groceryList, setGroceryList] = useState([]);
+    const [groceryListUpdate, setGroceryListUpdate] = useState({});
+    
+
+    const editGroceryList = (listo) => {
+        setGroceryListUpdate(listo);
+        console.log(listo);
+    }
+//fetchGroceryList() will be the GET Endpoint Function
     return (
-        <div className="main">
-        
-        
-        </div>
+        <Container>
+            <Row>
+                <Col md='3'>
+                    <GroceryListCreate fetchGroceryList={fetchGroceryList} token={props.sessionToken}/> 
+                </Col>
+                <Col md='6'>
+                    <GroceryListUpdate groceryList={groceryList} editGroceryList={editGroceryList} token={props.sessionToken}/>
+                </Col>
+            </Row>
+        </Container>
     )
 };
 
