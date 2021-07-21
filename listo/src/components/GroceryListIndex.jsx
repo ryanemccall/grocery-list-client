dc756bfff317f90d69f3606a5c29822159fd8bbe
Shannon D <sdillman@users.noreply.github.com>
2021-07-20 22:14:11 -0400

    undoing the wrong merge from a moment ago, GroceryListIndex

--- listo/src/components/GroceryListIndex.jsx
+++ listo/src/components/GroceryListIndex.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "reactstrap";
 import GroceryListUpdate from "./GroceryListUpdate";
 import GroceryListDelete from "./GroceryListDelete";
@@ -7,30 +7,69 @@ import GroceryListGet from "./GroceryListGet";
 
 const GroceryListIndex = (props) => {
     const [groceryList, setGroceryList] = useState([]);
-    const [groceryListUpdate, setGroceryListUpdate] = useState({});
-    
+    const [groceryListToUpdate, setGroceryListToUpdate] = useState({});
 
+    const [groceries, setGroceries] = useState([]);
+
+    const fetchGroceryList = () => {
+        fetch('http://localhost:3000/grocery', {
+            method: 'GET',
+            headers: new Headers ({
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${props.token}`
+            })
+        }).then( data => data.json())
+        .then( results => {
+            setGroceries(results);
+            console.log(results);
+        })
+        .catch(console.error);
+    };
+
+    const groceryListMapper = () => {
+        console.log(`ENTERED groceryListMapper`);
+        if (groceries.length > 0) {
+            return groceries.map((item, index) => {
+                return (
+                    <tr key={index}>
+                        <th scope="row">{item.id}</th>
+                        <td>{item.ingredient}</td>
+                        <td>{item.quantity}</td>
+                    </tr>
+                )
+            })
+        } else {
+        console.log("There are no groceries to map")
+        }
+    }
+    
+    useEffect(() => {
+        fetchGroceryList();
+    }, [props.token, props.groceryList])
+    
+   
     const editGroceryList = (listo) => {
-        setGroceryListUpdate(listo);
+        setGroceryListToUpdate(listo);
         console.log(listo);
     }
-//fetchGroceryList() will be the GET Endpoint Function
+   
     return (
         <Container>
             <Row>
                 <Col md='12'>
-                        <GroceryListGet token={props.token} groceryList={groceryList} /> 
+                    <GroceryListGet
+                        token={props.token}
+                        groceryList={groceryList}
+                        groceryListMapper={groceryListMapper} />
                 </Col>
-            </Row>
-            <Row>
-                <Col md="12">
-                    <GroceryListCreate token={props.token} />
+                <Col md='12'>
+                    <GroceryListCreate
+                        token={props.token} />
                 </Col>
-            </Row>
-            <Row>
-                
                 <Col md='6'>
-                    <GroceryListUpdate groceryList={groceryList} editGroceryList={editGroceryList} token={props.token}/>
+                    <GroceryListUpdate
+                        groceryListToUpdate={groceryListToUpdate}
+                        token={props.token} />
                 </Col>
             </Row>
         </Container>
@@ -38,4 +77,3 @@ const GroceryListIndex = (props) => {
 };
 
 export default GroceryListIndex;
-
